36b147cfcd5a97e37656f2638e4b783d5930557d
ZhouYuanZZZZ <1270157082@qq.com>
2018-03-13 18:37:56 +0800

    init

--- dataStruct/map/graphDef.c
+++
@@ -1,9 +0,0 @@
-#define VERTEX_NUM 20 //最大顶点数
-
-typedef enum {DG,DN,UDG,UDN} graphKind;//有项图,有项网
-
-typedef struct {
-
-    int vertel[VERTEX_NUM];      //顶点向量
-    int matrix[VERTEX_NUM][VERTEX_NUM];    //邻接矩阵
-} Graph;
